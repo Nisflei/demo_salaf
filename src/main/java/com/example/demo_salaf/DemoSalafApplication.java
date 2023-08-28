4311a5264eb6433f89619a4e5d389a74787a2570
Nisflei Galoni <88749138+Nisflei@users.noreply.github.com>
2023-08-28 22:49:58 +0000

    novo

--- src/main/java/com/example/demo_salaf/DemoSalafApplication.java
+++ src/main/java/com/example/demo_salaf/DemoSalafApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoSalafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSalafApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Germinare Tech - Nikinho";
 	}
 
 }
